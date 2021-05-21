d666b69e519c524e0c6627beb6db32713e2f1d3c
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-21 16:31:16 +0530

    Variables

--- userbot/mainfiles/config/variables.py
+++ userbot/mainfiles/config/variables.py
@@ -2,7 +2,7 @@
 
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
-    from userbot.mainfiles.var import Var as Config
+    from userbot.mainfiles.configs.heroku import Var as Config
 else:
     from local_config import Development as Config
 
