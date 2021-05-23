e6ac895e3bbd05f514c98033935ca365df1b93ab
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-23 18:29:08 +0530

    Okay

--- userbot/mainfiles/utils.py
+++ userbot/mainfiles/utils.py
@@ -55,7 +55,7 @@ def load_module(shortname):
         mod.tgbot = bot.tgbot
         mod.sudo_cmd = sudo_cmd
         mod.extremepro_cmd = admin_cmd
-        mod.amanpandey_cmd = amanpandey_cmd
+        mod.amanpandey_cmd = sudo_cmd
         mod.edit_or_reply = edit_or_reply
         mod.logger = logging.getLogger(shortname)
         # support for uniborg
