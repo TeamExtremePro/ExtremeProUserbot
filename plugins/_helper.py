35aeb9c7935371876da1e66611e1e82a380bbb86
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 17:40:10 +0530

    Update _helper.py

--- plugins/_helper.py
+++ plugins/_helper.py
@@ -1,6 +1,5 @@
 from plugins import CMD_LIST
 
-
 @command(pattern="^.help ?(.*)")
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
@@ -15,13 +14,22 @@ async def cmd_list(event):
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
-                await borg.send_message(event.chat_id, "Do .help cmd")
-                await asyncio.sleep(5)
+                with io.BytesIO(str.encode(string)) as out_file:
+                    out_file.name = "cmd.txt"
+                    await bot.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="**COMMANDS**",
+                        reply_to=reply_to_id
+                    )
+                    await event.delete()
             else:
                 await event.edit(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "Commands found in {}:\n".format(input_str)
+                string = "Commands found in {}:".format(input_str)
                 for i in CMD_LIST[input_str]:
                     string += "    " + i
                     string += "\n"
@@ -29,12 +37,15 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """ExtremePro Userbot Modules Are Listed Here !\n
-For More Help or Support Visit @ExtrePro"""
+            help_string = """Userbot Helper.. \nProvided by Extreme Pro userbot\n
+`Userbot Helper to reveal all the commands`"""
             results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername, help_string
+                tgbotusername,
+                help_string
             )
             await results[0].click(
-                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+                event.chat_id,
+                reply_to=event.reply_to_msg_id,
+                hide_via=True
             )
             await event.delete()
