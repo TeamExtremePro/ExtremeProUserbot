4b3693d8a3423f4fa6ca09b0d273b57b958ca62f
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 15:59:30 +0530

    Create __init__.py

---
+++ amanpandey/__init__.py
@@ -0,0 +1,10 @@
+import asyncio
+import os
+import asyncio
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+os.system("pip install Extre")
+from Extre import *
+from Extre.utils import admin_cmd, sudo_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from Extre.config import Config
+from Extre.variables import Var
