532564545759b50a63f1b0172728df432fa21b7d
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 18:12:12 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -10,4 +10,139 @@ from Extre.utils import admin_cmd, sudo_cmd, load_module, humanbytes, register, 
 from Extre.config import Config
 from Extre.variables import Var
 from Extre import CMD_LIST, CMD_HELP, CMD_HELP_BOT, BRAIN_CHECKER, INT_PLUG, LOAD_PLUG, COUNT_MSG, USERS, COUNT_PM, LASTMSG, CMD_HELP, ISAFK, AFKREASON, SUDO_LIST
+
 os.system("pip install catopbolte==0.1")
+import math
+import os
+import re
+import time
+
+import heroku3
+import lottie
+import requests
+import spamwatch as spam_watch
+from validators.url import url
+
+from userbot import *
+from userbot.helpers import *
+from userbot.helpers import _cattools, _catutils, _format
+
+# =================== CONSTANT ===================
+
+USERID = bot.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
+ALIVE_NAME = Config.ALIVE_NAME
+AUTONAME = Config.AUTONAME
+DEFAULT_BIO = Config.DEFAULT_BIO
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
+BOT_USERNAME = Config.TG_BOT_USERNAME
+# mention user
+mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
+hmention = f"<a href = tg://user?id={USERID}>{DEFAULTUSER}</a>"
+
+
+Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+HEROKU_APP_NAME = Config.HEROKU_APP_NAME
+HEROKU_API_KEY = Config.HEROKU_API_KEY
+
+thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
+
+PM_START = []
+PMMESSAGE_CACHE = {}
+PMMENU = "pmpermit_menu" not in Config.NO_LOAD
+
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    BOTLOG = False
+    BOTLOG_CHATID = "me"
+else:
+    BOTLOG = True
+    BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+
+# Gdrive
+G_DRIVE_CLIENT_ID = Config.G_DRIVE_CLIENT_ID
+G_DRIVE_CLIENT_SECRET = Config.G_DRIVE_CLIENT_SECRET
+G_DRIVE_DATA = Config.G_DRIVE_DATA
+G_DRIVE_FOLDER_ID = Config.G_DRIVE_FOLDER_ID
+TMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
+
+# spamwatch support
+if Config.SPAMWATCH_API:
+    token = Config.SPAMWATCH_API
+    spamwatch = spam_watch.Client(token)
+else:
+    spamwatch = None
+
+cat_users = [bot.uid]
+if Config.SUDO_USERS:
+    for user in Config.SUDO_USERS:
+        cat_users.append(user)
+
+
+# ================================================
+
+if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+    os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+
+
+# thumb image
+if Config.THUMB_IMAGE is not None:
+    check = url(Config.THUMB_IMAGE)
+    if check:
+        try:
+            with open(thumb_image_path, "wb") as f:
+                f.write(requests.get(Config.THUMB_IMAGE).content)
+        except Exception as e:
+            LOGS.info(str(e))
+
+
+def check(cat):
+    if "/start" in cat:
+        return True
+    try:
+        hi = re.search(cat.lower(), "(a|b|c|d)", flags=re.IGNORECASE)
+    except Exception:
+        hi = False
+    return bool(hi)
+
+
+def set_key(dictionary, key, value):
+    if key not in dictionary:
+        dictionary[key] = value
+    elif isinstance(dictionary[key], list):
+        if value in dictionary[key]:
+            return
+        dictionary[key].append(value)
+    else:
+        dictionary[key] = [dictionary[key], value]
+
+
+# UniBorg Telegram UseRBot
+# Copyright (C) 2020 @UniBorg
+# This code is licensed under
+# the "you can't use this for anything - public or private,
+# unless you know the two prime factors to the number below" license
+# 543935563961418342898620676239017231876605452284544942043082635399903451854594062955
+# വിവരണം അടിച്ചുമാറ്റിക്കൊണ്ട് പോകുന്നവർ
+# ക്രെഡിറ്റ് വെച്ചാൽ സന്തോഷമേ ഉള്ളു..!
+# uniborg
+
+
+def check_data_base_heal_th():
+    # https://stackoverflow.com/a/41961968
+    is_database_working = False
+    output = "No Database is set"
+    if not Config.DB_URI:
+        return is_database_working, output
+    from userbot.plugins.sql_helper import SESSION
+
+    try:
+        # to check database we will execute raw query
+        SESSION.execute("SELECT 1")
+    except Exception as e:
+        output = f"❌ {str(e)}"
+        is_database_working = False
+    else:
+        output = "Functioning"
+        is_database_working = True
+    return is_database_working, output
+
