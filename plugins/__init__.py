32debf753b06d877f6bf98ecf06690150e4c9bcc
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 17:31:04 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -9,3 +9,4 @@ from Extre import *
 from Extre.utils import admin_cmd, sudo_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from Extre.config import Config
 from Extre.variables import Var
+from Extre import CMD_LIST, CMD_HELP, CMD_HELP_BOT, BRAIN_CHECKER, INT_PLUG, LOAD_PLUG, COUNT_MSG, USERS, COUNT_PM, LASTMSG, CMD_HELP, ISAFK, AFKREASON, SUDO_LIST
