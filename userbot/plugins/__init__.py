8c404b89a7cb3cf90b2ac8ff0ffaa44b9466e964
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 09:24:38 +0530

    okay

---
+++ userbot/plugins/__init__.py
@@ -0,0 +1,10 @@
+import asyncio
+import os
+import asyncio
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+os.system("pip install AmanPandey")
+from AmanPandey import *
+from AmanPandey.utils import admin_cmd, sudo_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from AmanPandey.config import Config
+from AmanPandey.variables import Var
