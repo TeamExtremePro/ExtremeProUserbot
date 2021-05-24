e57148937d7cc99f0336a568acc1d7f24946e9c2
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 08:57:44 +0530

    Create __init__.py

---
+++ userbot/__init__.py
@@ -0,0 +1,10 @@
+import asyncio
+import os
+import asyncio
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+os.system("pip install Extreme-Pro")
+from Extreme-Pro import *
+from Extreme-Pro.utils import admin_cmd, sudo_cmd
+from Extreme-Pro.config import Config
+from Extreme-Pro.variables import Var
