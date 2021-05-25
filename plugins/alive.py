126480dc291e923a99141fcf517d74c9a5982f86
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-25 15:00:45 +0530

    Okay

--- plugins/alive.py
+++ plugins/alive.py
@@ -1,68 +1,45 @@
-import asyncio
-import random
-from telethon import events
-from amanpandey import extremepro_cmd
-from telethon.tl.types import ChannelParticipantsAdmins
-# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
-
-edit_time = 5
-""" =======================CONSTANTS====================== """
-file1 = "https://telegra.ph/file/7486dce0f715ccc0576b6.jpg"
-file2 = "https://telegra.ph/file/cc44dbb6c06fae646da13.jpg"
-file3 = "https://telegra.ph/file/4189164cadd30a8a2d6da.jpg"
-file4 = "https://telegra.ph/file/1b6d7ec6c2382fb80a094.jpg"
-file5 = "https://telegra.ph/file/d57626f8b84037d156d88.jpg"
-file6 = "https://telegra.ph/file/5c0bd9eacf8789ab4f4c3.jpg"
-file7 = "https://telegra.ph/file/33727f0de96eb4affc714.jpg"
-pm_caption = "🔥🔥 **DYNAMIC IS WORKING FINE LIKE MY OWNER..!! **🔥🔥\n\n"
-pm_caption += "⚔️⚔️ ** REAL OWNER AND BOT CODER TEAM DYNAMIC**⚔️⚔️\n\n"
-pm_caption += "◆◆S𝚈𝚂𝚃𝙴𝙼 𝚂𝚃𝙰𝚃𝚄𝚂◆◆◆\n\n"
-pm_caption += "●●𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ●● : 1.21.1\n\n"
-pm_caption += "●●  DYNAMIC VERSION ●●>> :1.0 Stable \n\n"
-pm_caption += " PYTHON VERSION : 3.9.2 \n\n"
-pm_caption += " DISK USAGE : 500 GB/1.5 TB \n\n"
-pm_caption += " DYNAMIC SOFTWARE : STABLE VERSION \n\n"
-pm_caption += "EVERTHING IS FINE \n\n"
-@borg.on(extremepro_cmd(pattern=r"alive"))
+#MADE BY PERRY_XD
+#MADE FOR EXTREMEPRO USERBOT
+#WITHOUT CREDITS KANG KIYA TO ZINDA GAADH DUNGA BHOSDK
 
 
+from telethon import Button, custom
+import re, os
+from amanpandey import extremepro_cmd as perry_cmd, amanpandey_cmd as god_cmd
+from amanpandey import bot as perry
+from Extre import ALIVE_NAME
 
-async def amireallyalive(yes):
-    chat = await yes.get_chat()
-
-    on = await borg.send_file(yes.chat_id, file=file1,caption=pm_caption)
-
-    await asyncio.sleep(edit_time)
-    ok = await borg.edit_message(yes.chat_id, on, file=file2) 
-
-    await asyncio.sleep(edit_time)
-    ok2 = await borg.edit_message(yes.chat_id, ok, file=file3)
-
-    await asyncio.sleep(edit_time)
-    ok3 = await borg.edit_message(yes.chat_id, ok2, file=file1)
+@perry.on(perry_cmd(pattern="alive"))
+@perry.on(god_cmd(pattern="alive'", allow_sudo=True))
+async def godboy(event):
+	Extreme = "HELLO SIR I AM AN EXTREMEPRO USERBOT powered by TEAM-EXTREMEPRO \n\n"
+	Extreme += "Telethon Version : 1.21.1\n\n"
+	Extreme += "Python Version : 3.9.2\n\n"
+	BUTTON = [[Button.url("My Master", f"https://t.me/{bot.me.username}"), Button.url(f" EXTREMEPRO 𝚁𝙴𝙿𝙾", "https://github.com/TeamExtremePro/ExtremeProUserbot")]]
+    BUTTON += [[custom.Button.inline("𝚁epository", data="Extreme")]]
+    await perry.send_file(event.chat_id, PHOTO, caption=Extreme,  buttons=BUTTON)
+  
+  
+@perry.on(events.callbackquery.CallbackQuery(data=re.compile(b"Extreme")))
+async def nikkal(event):
+  GodBot = [[Button.url("REPO-EXTREMEPRO", "https://github.com/TeamExtremePro/ExtremeProUserbot")]]
+  GodBot +=[[Button.url("DEPLOY-EXTREMEPRO", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FExtremeProUserbot&template=https%3A%2F%2Fgithub.com%2FExtremeProUserbot%2FExtremeProUserbot")]]
+  GodBot +=[[Button.url("API_ID & HASH", "https://t.me/ScrapperRoBot")
+  GodBot +=[[custom.Button.inline("ALIVE", data="GodBot")]]
+  await event.edit(text=f"Detailed form of Repo", buttons=GodBot)
+  
+  
+  
+@perry.on(events.callbackquery.CallbackQuery(data=re.compile(b"GodBot")))
+async def chal(event):
+    Extreme = "HELLO SIR I AM AN EXTREMEPRO USERBOT powered by TEAM-EXTREMEPRO \n\n"
+	Extreme += "Telethon Version : 1.21.1\n\n"
+	Extreme += "Python Version : 3.9.2\n\n"
+	BUTTON = [[Button.url("My Master", f"https://t.me/{bot.me.username}"), Button.url(f" EXTREMEPRO 𝚁𝙴𝙿𝙾", "https://github.com/TeamExtremePro/ExtremeProUserbot")]]
+    BUTTON += [[custom.Button.inline("𝚁epository", data="Extreme")]]
+    await event.edit(text=Extreme, buttons=BUTTON)
     
-    await asyncio.sleep(edit_time)
-    ok4 = await borg.edit_message(yes.chat_id, ok3, file=file3)
     
-    await asyncio.sleep(edit_time)
-    ok5 = await borg.edit_message(yes.chat_id, ok4, file=file2)
-    
-    await asyncio.sleep(edit_time)
-    ok6 = await borg.edit_message(yes.chat_id, ok5, file=file1)
-    
-    await asyncio.sleep(edit_time)
-    ok7 = await borg.edit_message(yes.chat_id, ok6, file=file4)
-    
-    await asyncio.sleep(edit_time)
-    ok8 = await borg.edit_message(yes.chat_id, ok7, file=file5)
-      
-    await asyncio.sleep(edit_time)
-    ok9 = await borg.edit_message(yes.chat_id, ok8, file=file6)
-    
-    await asyncio.sleep(edit_time)
-    ok10 = await borg.edit_message(yes.chat_id, ok9, file=file7)
-
-    
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
-    await alive.delete()
+@perry.on(events.NewMessage(pattern="repo"))
+async def repo(event):
+  await perry.send_message(event.chat, buttons=[[Button.url("Repo", "https://github.com/TeamExtremePro/ExtremeProUserbot")]])
