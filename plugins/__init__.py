187238a35063da9be26ba2d7d2146550bd9125bb
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-25 14:41:36 +0530

    Life And Game Changer Codes 😂😂❤❤ Keep Devloping

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,7 +6,7 @@ import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from amanpandey import *
-from amanpandey import admin_cmd, sudo_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from amanpandey import extremepro_cmd, amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from amanpandey import Config
 from amanpandey import Var
 from Extre import CMD_LIST, CMD_HELP, CMD_HELP_BOT, BRAIN_CHECKER, INT_PLUG, LOAD_PLUG, COUNT_MSG, USERS, COUNT_PM, LASTMSG, CMD_HELP, ISAFK, AFKREASON, SUDO_LIST
