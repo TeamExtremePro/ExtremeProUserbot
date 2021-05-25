34156fa0fda4c6dfc69ff5c7f326c152dffe9e5d
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-25 14:40:38 +0530

    Life Changer

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -5,6 +5,6 @@ import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from amanpandey import *
-from amanpandey import admin_cmd, sudo_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from amanpandey import extremepro_cmd, amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from amanpandey import Config
 from amanpandey import Var
