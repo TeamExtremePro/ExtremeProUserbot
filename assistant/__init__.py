3ff91b63c3ed19444363d564269273eb3d7a73a2
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-25 14:37:16 +0530

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -4,7 +4,7 @@ import os
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from Extre import *
-from Extre.utils import admin_cmd, sudo_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
-from Extre.config import Config
-from Extre.variables import Var
+from amanpandey import *
+from amanpandey import admin_cmd, sudo_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from amanpandey import Config
+from amanpandey import Var
