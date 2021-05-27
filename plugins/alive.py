7b8f04a2ce59eace880ac63700a30cef4591e04d
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-27 10:00:17 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -1,1 +1,20 @@
+import asyncio
+import random
+from telethon import events
+from amanpandey import extremepro_cmd
+from telethon.tl.types import ChannelParticipantsAdmins
+# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
 
+edit_time = 5
+""" =======================CONSTANTS====================== """
+file1 = "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
+pm_caption = "Userbot is Alive ⚙️\n\n"
+pm_caption += "●● VERSION  :0.1  \n\n"
+pm_caption += "●● PYTHON : 3.9.2 \n\n"
+pm_caption += "●● TELETHON : 1.21.1\n\n"
+pm_caption += "●●  ExtremePro VERSION ●●>> :0.1 \n\n"
+@borg.on(extremepro_cmd(pattern=r"alive")
+
+    
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
