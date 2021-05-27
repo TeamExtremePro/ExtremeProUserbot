9f7dcf776fcfe93498fc24d4f6d1fa5af4d65d40
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-27 09:43:01 +0530

    Test Sucess

--- amanpandey/__init__.py
+++ amanpandey/__init__.py
@@ -1,9 +1,12 @@
+# Copright Team ExtremePro (C) 2021-2022
 import asyncio
 import os
 import asyncio
+os.system("pip install --upgrade telethon")
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from main_startup import *
-from main_startup import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
-from main_startup import Config
-from main_startup import Var
+os.system("pip install --upgrade Extre")
+from Extre import *
+from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from Extre.config import Config
+from Extre.variables import Var
