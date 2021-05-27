c70d71293c6b55bcf64b0a23476c96475819fe42
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-27 09:23:18 +0530

    Bruh

--- amanpandeyhero.py
+++ amanpandeyhero.py
@@ -1,4 +1,3 @@
-# Speical Thanks To Shivam Patel For this Fix And team Javes Armaan The Gamer King 
 # COPYRIGHT BY TEAM EXTREMEPRO (C) 2021-2022
 import os
 os.system("git clone https://github.com/TeamExtremePro/ExtremeProUserbot /root/main_startup && mkdir /root/userbot/bin/ && cd /root/main_startup/ && mv Resources/apktool /usr/local/bin && mv Resources/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m main_startup")
