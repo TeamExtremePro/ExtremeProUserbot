11dea76bc6fb35a2b58fb46c35cea509312555c1
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-27 09:39:23 +0530

    nah nahi hoga mere se fucked and ripped off

--- amanpandeyhero.py
+++
@@ -1,3 +0,0 @@
-# COPYRIGHT BY TEAM EXTREMEPRO (C) 2021-2022
-import os
-os.system("git clone https://github.com/TeamExtremePro/ExtremeProUserbot /root/main_startup && mkdir /root/userbot/bin/ && cd /root/main_startup/ && mv Resources/apktool /usr/local/bin && mv Resources/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m main_startup")
