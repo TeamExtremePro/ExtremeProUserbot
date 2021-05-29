03aedc0691f2c1f92f77e8a4fd2416e466f544df
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-29 09:00:40 +0530

    Update __init__.py

--- amanpandey/__init__.py
+++ amanpandey/__init__.py
@@ -7,7 +7,7 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 os.system("pip install --upgrade Extre")
 from Extre import *
-from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, tgbot, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from Extre.config import Config
 from Extre.variables import Var
 bot = "ExtremeProUserBot"
