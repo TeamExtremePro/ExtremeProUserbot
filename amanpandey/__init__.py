36a77b81e355dcdf4756759440fb53cc1ae9f15e
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-29 08:50:15 +0530

    Ok

--- amanpandey/__init__.py
+++ amanpandey/__init__.py
@@ -4,10 +4,10 @@ import os
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from Extre import *
-from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
-from Extre.config import Config
-from Extre.variables import Var
+from userbot import *
+from userbot import extremepro_cmd, amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from userbot import Config
+from userbot import Var
 bot = "ExtremeProUserBot"
 
 
