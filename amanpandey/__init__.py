87d5e663919770fcc16ca88bf8a168c8bd99580c
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-29 08:36:54 +0530

    Added something New

--- amanpandey/__init__.py
+++ amanpandey/__init__.py
@@ -8,3 +8,8 @@ from Extre import *
 from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from Extre.config import Config
 from Extre.variables import Var
+bot = "ExtremeProUserBot"
+
+
+# Default .alive name
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
