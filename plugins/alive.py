25cd556b8a39faa30404e010374469869efafca1
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-05-31 16:31:12 +0530

    Alive

--- plugins/alive.py
+++ plugins/alive.py
@@ -1,6 +1,6 @@
 import asyncio
 import random
-from telethon import events
+from telethon import events, TelegramClient
 from amanpandey import extremepro_cmd
 from telethon.tl.types import ChannelParticipantsAdmins
 # 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
@@ -8,10 +8,10 @@ from telethon.tl.types import ChannelParticipantsAdmins
 edit_time = 5
 """ =======================CONSTANTS====================== """
 PM_IMG = "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
-pm_caption = "ExtremePro is Alive ⚙️\n\n"
-pm_caption += "●● VERSION  :0.1  \n\n"
-pm_caption += "●● PYTHON : 3.9.2 \n\n"
-pm_caption += "●● TELETHON : 1.21.1\n\n"
+pm_caption = " ●● ExtremePro is Alive ●●\n\n"
+pm_caption += "●● VERSION  :0.1 ●●\n\n"
+pm_caption += "●● PYTHON : 3.9.2 ●●\n\n"
+pm_caption += "●● TELETHON : 1.21.1 ●●\n\n"
 pm_caption += "●● Copyright Team ExtremePro ●● :2021-2022 \n\n"
 pm_caption += "●● Extreme Branch ●● :Main \n\n"
 
