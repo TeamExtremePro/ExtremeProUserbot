f389e93bed2ca5f025b3aaae5e183b4601b313c6
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-06-04 15:01:14 +0530

    Ok

---
+++ pmpermit/exclusive.py
@@ -0,0 +1,11 @@
+import os
+
+STICKER_PACK_NAME = os.environ.get("STICKER_PACK_NAME", "Creepy Stickers")
+
+ANIMATED_STICKER_PNAME = os.environ.get("ANIMATED_STICKER_PNAME", "Creepy Animated Stickers")
+
+AUTONAME = os.environ.get("AUTONAME", None)
+
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/ExtremeProusers/2")
+
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
