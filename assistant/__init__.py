aa1486b797b618dad2f85a2132b9b2d87eb85cd1
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-04 16:37:49 +0530

    Extreme Pro Developer Version 0.1

---
+++ assistant/__init__.py
@@ -0,0 +1,10 @@
+# ASSISTANT IMPORT
+import asyncio
+import os
+import asyncio
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from amanpandey import *
+from amanpandey import extremepro_cmd, amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from amanpandey import Config
+from amanpandey import Var
---
+++ assistant/hmm.py
@@ -0,0 +1,1 @@
+## JANA YAAR La Raha Hu Baad Me Ho Sakta hai Na Bhi Lau Kya Karna hai
