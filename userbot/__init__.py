8c9ae5bf8d8b10d800855f91ae372218ed10f55d
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-06-04 15:15:15 +0530

    Ok Added Bot

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -11,3 +11,4 @@ from Extre import *
 from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from Extre.config import Config
 from Extre.variables import Var
+bot = "EXTREMEPRO USERBOT"
