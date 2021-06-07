01b5d76a057860c69b2ee3d8597b7c26dca6123c
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-06-07 16:46:39 +0530

    Developer test Version 0.1

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,8 +5,7 @@ import os
 import asyncio
 os.system("pip install --upgrade telethon")
 from telethon import TelegramClient
-from telethon.sessions import StringSession
-os.system("pip install --upgrade Extre")
+from telethon.sessions import StringSession)
 from Extre import *
 from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from Extre.config import Config
