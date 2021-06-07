678886f61052f7ba209ecb144185e046568d0d22
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-06-07 16:51:14 +0530

    Developer Experience

--- amanpandey/__init__.py
+++ amanpandey/__init__.py
@@ -5,7 +5,6 @@ import asyncio
 os.system("pip install --upgrade telethon")
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-os.system("pip install --upgrade Extre")
 from Extre import *
 from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin, edit_or_reply
 from Extre.config import Config
