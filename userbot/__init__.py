2364d8a996ace3d896243d424194ef39548973bf
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-08 12:10:25 +0530

    added new

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -7,6 +7,7 @@ os.system("pip install --upgrade telethon")
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 os.system("pip install --upgrade Extre")
+os.system("pip install --upgrade ExtreMods")
 from Extre import *
 from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from Extre.config import Config
