2d8237e39eea23601bf245cdc43ca3c8b39217b3
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-11 18:56:25 +0530

    jnl

--- plugins/sudo.py
+++ plugins/sudo.py
@@ -2,7 +2,7 @@ import os
 import heroku3
 from telethon.tl.functions.users import GetFullUserRequest
 
-from . import *
+from plugins import *
 
 Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
