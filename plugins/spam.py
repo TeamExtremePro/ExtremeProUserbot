dfb9ebd017743c76db69859ff120d8118210aced
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-11 19:19:58 +0530

    sudo

--- plugins/spam.py
+++ plugins/spam.py
@@ -10,6 +10,7 @@ from telethon import events
 
 
 @borg.on(events.NewMessage(pattern=r"\.spam", outgoing=True))
+@borg.on(amanpandey_cmd("spam ?(.*)", allow_sudo=True))
 async def spammer(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
