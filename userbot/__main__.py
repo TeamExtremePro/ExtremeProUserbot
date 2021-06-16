8e6976f01522a48d4df7c47eb35bfef42b1fb70f
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-16 09:03:46 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,7 +8,7 @@ from telethon.sessions import StringSession
 from Extre import *
 from Extre.utils import admin_cmd as extremepro_cmd, sudo_cmd as amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from Extre.config import Config
-from Extre.var import Var
+from Extre.variables import Var
 from Extre import bot
 from sys import argv
 import sys
