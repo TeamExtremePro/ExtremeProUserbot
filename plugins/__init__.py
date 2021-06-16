69ad650b2241b84a62ae55f599ada61fb1109e5c
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-16 21:36:09 +0530

    init

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,3 +11,61 @@ from amanpandey import Config
 from amanpandey import Var
 from Extre import CMD_LIST, CMD_HELP, CMD_HELP_BOT, BRAIN_CHECKER, INT_PLUG, LOAD_PLUG, COUNT_MSG, USERS, COUNT_PM, LASTMSG, CMD_HELP, ISAFK, AFKREASON, SUDO_LIST
 AUTONAME = os.environ.get("AUTONAME", None)
+from telethon.tl.types import Channel
+
+from amanpandey import *
+from amanpandey import ALIVE_NAME, bot, telever
+from amanpandey import Config, Var
+
+# stats
+if Var.PRIVATE_GROUP_ID:
+    log = "Enabled"
+else:
+    log = "Disabled"
+
+if Config.TG_BOT_USER_NAME_BF_HER:
+    bots = "Enabled"
+else:
+    bots = "Disabled"
+
+if Var.LYDIA_API_KEY:
+    lyd = "Enabled"
+else:
+    lyd = "Disabled"
+
+if Config.SUDO_USERS:
+    sudo = "Disabled"
+else:
+    sudo = "Enabled"
+
+if Var.PMSECURITY.lower() == "off":
+    pm = "Disabled"
+else:
+    pm = "Enabled"
+
+TELEUSER = str(ALIVE_NAME) if ALIVE_NAME else "EXTREMEPRO USER"
+
+extremepro = f"EXTREMEPRO Version: {extremever}\n"
+extremepro += f"Log Group: {log}\n"
+extremepro += f"Assistant Bot: {bots}\n"
+extremepro += f"Lydia: {lyd}\n"
+extremepro += f"Sudo: {sudo}\n"
+extremepro += f"PMSecurity: {pm}\n"
+extremepro += f"\nVisit @TeleBotSupport for assistance.\n"
+extremeprostats = f"{tele}"
+
+EXTREMEPRO_NAME = bot.me.first_name
+OWNER_ID = bot.me.id
+
+# count total number of groups
+
+
+async def extremepro_grps(event):
+    a = []
+    async for dialog in event.client.iter_dialogs():
+        entity = dialog.entity
+        if isinstance(entity, Channel):
+            if entity.megagroup:
+                if entity.creator or entity.admin_rights:
+                    a.append(entity.id)
+    return len(a), a
