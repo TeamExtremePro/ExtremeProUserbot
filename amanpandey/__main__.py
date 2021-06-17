c64886687e40218c2ebe899a5b12119bcfe48675
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-17 13:11:29 +0530

    __main__.py

--- amanpandey/__main__.py
+++ amanpandey/__main__.py
@@ -34,7 +34,7 @@ if len(argv) not in (1, 3, 4):
 else:
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        print("userbot starting telethon")
+        print("Connecting To Scratch Server")
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
             "TG_BOT_TOKEN",
@@ -42,6 +42,7 @@ else:
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Connected To Scratch Server")
     else:
         bot.start()
 import glob
@@ -67,7 +68,7 @@ for name in files:
 
 
 
-print("USERBOT STARTED  ")
+print("USERBOT Deployed And Working Fine For Assistance Join @EXTREMEPROUSERBOTSUPPORT ")
 
 
 
