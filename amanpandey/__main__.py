d55b6e9e05136bd53ef25293462e58b9bc777d77
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-17 15:40:39 +0530

    iniy

--- amanpandey/__main__.py
+++ amanpandey/__main__.py
@@ -56,13 +56,13 @@ for name in files:
         start_assistant(shortname.replace(".py", ""))   
 
 import glob
-os.system("git clone https://github.com/TeamExtremePro/MODULES.git ./plugins/")
-path = "plugins/*.py"
-files = glob.glob(path)
-for name in files:
-     with open(name) as a:
-         patt = Path(a.name)
-            shortname = path1.stem
+    os.system("git clone https://github.com/TeamExtremePro/MODULES.git ./plugins/")
+    path = "plugins/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem
             try:
                 load_module(plugin_name.replace(".py", ""))
                 if not plugin_name.startswith("__") or plugin_name.startswith("_"):
