f13f02a985d1df1d763bd7cb3c13490f223ff298
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-17 09:00:45 +0530

    Update pmsecurity.py

--- plugins/pmsecurity.py
+++ plugins/pmsecurity.py
@@ -287,5 +287,4 @@ CMD_HELP.update(
         \n\n.disapprove/.da\nUse - DisApprove PM\
         \n\n.listapproved\nUse - Get all approved PMs.\
         \n\nSet var PMPERMIT_PIC for custom PMPic, CUSTOM_PMPERMIT for custom text, PMSECURITY <on/off> to enable/disable, INSTANT_BLOCK <on/off>.\
-        \nGet help from EXTREMEPROBotHelpBot."
-    }
+        \nGet help from EXTREMEPROBotHelpBot."}
