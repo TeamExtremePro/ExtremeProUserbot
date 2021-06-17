87e2a3d068baa9535de0acd84edd0b9fd285b67b
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-17 18:18:44 +0530

    fed

--- sql_helper/feds_sql.py
+++ sql_helper/feds_sql.py
@@ -1,7 +1,7 @@
-# COPYRIGHT (C) 2021-22 BY @GODBOYX AND TEAM-DYNAMIC
+# COPYRIGHT (C) 2021-22 BY @GODBOYX AND TEAM EXTREMEPRO
 # YOU CAN COPY BUT DONT DARE TO REMOVE THESE LINES
 import threading
-from DYNAMIC.plugins.sql_helper import BASE, SESSION
+from sql_helper import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
