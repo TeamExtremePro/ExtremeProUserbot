bb951cc2a8acbabc6cb9ad138c39175c57d92bbc
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-17 18:21:01 +0530

    m

--- sql_helper/filter_sql.py
+++ sql_helper/filter_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, UnicodeText, LargeBinary, Numeric, String
-from DYNAMIC.plugins.sql_helper import SESSION, BASE
+from sql_helper import SESSION, BASE
 
 
 class Filters(BASE):
