d1d24f7e4ada07d27bf27b30e3779ee57f03bd34
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-18 19:02:33 +0530

    alive

--- plugins/alive.py
+++ plugins/alive.py
@@ -9,7 +9,7 @@ from amanpandey import extremepro_cmd, amanpandey_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 """ =======================CONSTANTS====================== """
-EXTREMEPRO_PIC = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
+EXTREMEPRO_PIC = os.environ.get("EXTREMEPRO_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
 EXTREMEPRO = "**`I Am Alive Master.`**\n\n"
 EXTREMEPRO += "**✅Telethon version:- 1.21.1**\n"
 EXTREMEPRO += "**✅Python: 3.9.5\n\n"
