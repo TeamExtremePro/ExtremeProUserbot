f600b74988b407388b3fef8f7bb518702ada5a8f
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-18 18:55:42 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -9,21 +9,19 @@ from amanpandey import extremepro_cmd, amanpandey_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 """ =======================CONSTANTS====================== """
-AMANPANDEY = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
+EXTREMEPRO_PIC = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
 EXTREMEPRO = "**`I Am Alive Master.`**\n\n"
 EXTREMEPRO += "**✅Telethon version:- 1.21.1**\n"
 EXTREMEPRO += "**✅Python: 3.9.5\n\n"
 EXTREMEPRO += "**✅Database Status: Databases functioning normally!**\nSQL\n"
 EXTREMEPRO += f"`Owner`: {DEFAULTUSER}\n"
 EXTREMEPRO += "[ExtremeProUserBot](https://github.com/TeamExtremePro/ExtremeProUserbot)"
-@borg.on(extremepro_cmd(pattern=r"alive"))
-@borg.on(amanpandey_cmd(pattern=r"alive", allow_sudo=True))
-async def repo(event):
-    if event.fwd_from:
+@borg.on(extremepro_cmd(outgoing=True, pattern="alive$"))
+@borg.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+async def up(op):
+    if op.fwd_from:
         return
-    amanpandey = Var.TG_BOT_USER_NAME_BF_HER
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    response = await bot.inline_query(amanpandey, "alive")
-    await response[0].click(event.chat_id)
-    await event.delete()
+    await op.get_chat()
+    await op.delete()
+    await borg.send_file(op.chat_id, EXTREMEPRO_PIC, caption=EXTREMEPRO)
+    await op.delete()
