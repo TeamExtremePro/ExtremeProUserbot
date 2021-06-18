7b7d141d90e6d9d61a3a94ad10a173bd71c86371
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-18 18:37:26 +0530

    li

--- plugins/alive.py
+++ plugins/alive.py
@@ -10,11 +10,11 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 """ =======================CONSTANTS====================== """
 AMANPANDEY = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
 EXTREMEPRO = "**`I Am Alive Master.`**\n\n"
-EXTREMEPRO = "**✅Telethon version:- 1.21.1**\n"
-EXTREMEPRO + "**✅Python: 3.9.5\n\n"
-EXTREMEPRO = "**✅Database Status: Databases functioning normally!**\nSQL\n"
-EXTREMEPRO = f"`Owner`: {DEFAULTUSER}\n"
-EXTREMEPRO = "[ExtremeProUserBot](https://github.com/TeamExtremePro/ExtremeProUserbot)"
+EXTREMEPRO += "**✅Telethon version:- 1.21.1**\n"
+EXTREMEPRO += "**✅Python: 3.9.5\n\n"
+EXTREMEPRO += "**✅Database Status: Databases functioning normally!**\nSQL\n"
+EXTREMEPRO += f"`Owner`: {DEFAULTUSER}\n"
+EXTREMEPRO += "[ExtremeProUserBot](https://github.com/TeamExtremePro/ExtremeProUserbot)"
 @borg.on(extremepro_cmd(pattern=r"alive"))
 @borg.on(amanpandey_cmd(pattern=r"alive", allow_sudo=True))
 async def amanpandey(alive):
