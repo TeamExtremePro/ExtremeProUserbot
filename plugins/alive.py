26571ee556cc192a1fea8c45b50dea0d55e86b5c
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-18 18:17:35 +0530

    alive

--- plugins/alive.py
+++ plugins/alive.py
@@ -10,6 +10,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 @borg.on(extremepro_cmd(pattern=r"alive"))
 @borg.on(amanpandey_cmd(pattern=r"alive", allow_sudo=True))
 async def amanpandey(alive):
+    ALIVE_PIC = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**`I Am Alive Master.`**\n\n"
                      "**✅Telethon version:- 1.21.1**\n"
