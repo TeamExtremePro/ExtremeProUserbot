7d6d0a9207a5e30935be95db4c8970bcdf2163b1
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-18 18:30:38 +0530

    ALIVE

--- plugins/alive.py
+++ plugins/alive.py
@@ -6,15 +6,15 @@ from amanpandey import ALIVE_NAME
 from amanpandey import extremepro_cmd, amanpandey_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
-
+""" =======================CONSTANTS====================== """
+AMANPANDEY = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
+EXTREMEPRO = "**`I Am Alive Master.`**\n\n"
+EXTREMEPRO = "**✅Telethon version:- 1.21.1**\n"
+EXTREMEPRO + "**✅Python: 3.9.5\n\n"
+EXTREMEPRO = "**✅Database Status: Databases functioning normally!**\nSQL\n"
+EXTREMEPRO = f"`Owner`: {DEFAULTUSER}\n"
+EXTREMEPRO = "[ExtremeProUserBot](https://github.com/TeamExtremePro/ExtremeProUserbot)")
 @borg.on(extremepro_cmd(pattern=r"alive"))
 @borg.on(amanpandey_cmd(pattern=r"alive", allow_sudo=True))
 async def amanpandey(alive):
-    AMANPANDEY = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
-    """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`I Am Alive Master.`**\n\n"
-                     "**✅Telethon version:- 1.21.1**\n"
-                     "**✅Python: 3.9.5\n\n"
-                     "**✅Database Status: Databases functioning normally!**\nSQL\n"
-                     f"`Owner`: {DEFAULTUSER}\n"
-                     "[ExtremeProUserBot](https://github.com/TeamExtremePro/ExtremeProUserbot)")
+    await borg.send_file(event.chat_id, AMANPANDEY, EXTREMEPRO)
