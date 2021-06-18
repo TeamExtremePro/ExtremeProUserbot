dc1326b2bbfd325a379d5b6c33ba99ab3bb8c5d0
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-18 15:33:42 +0530

    alive testing

--- plugins/alive.py
+++ plugins/alive.py
@@ -1,23 +1,19 @@
-import asyncio
-import random
-from telethon import events, TelegramClient
-from amanpandey import extremepro_cmd
-from telethon.tl.types import ChannelParticipantsAdmins
-# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
 
-edit_time = 5
-""" =======================CONSTANTS====================== """
-PM_IMG = "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
-pm_caption = " ⚡ ExtremePro is Alive ⚡\n\n"
-pm_caption += "⚡ VERSION  :0.1 ⚡\n\n"
-pm_caption += "⚡ PYTHON : 3.9.5 ⚡\n\n"
-pm_caption += "⚡ PIP : 21.1.2 ⚡\n\n"
-pm_caption += "⚡ TELETHON : 1.21.1 ⚡\n\n"
-pm_caption += "⚡ DATABASE : SQL == MONGODB ⚡\n\n"
-pm_caption += "⚡ Copyright Team ExtremePro ⚡ :2021-2022 \n\n"
-pm_caption += "⚡ Extreme Branch ⚡ :DEV \n\n"
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from amanpandey import ALIVE_NAME
+from amanpandey import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 
 @borg.on(extremepro_cmd(pattern=r"alive"))
 @borg.on(amanpandey_cmd(pattern=r"alive", allow_sudo=True))
-async def amanpandey(event):
-    await borg.send_file(event.chat_id, PM_IMG, caption=pm_caption)
+async def amanpandey(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("**`I Am Alive Master.`**\n\n"
+                     "**✅Telethon version:- 1.21.1**\n"
+                     "**✅Python: 3.9.5\n\n"
+                     "**✅Database Status: Databases functioning normally!**\nSQL\n"
+                     f"`Owner`: {DEFAULTUSER}\n"
+                     "[ExtremeProUserBot](https://github.com/TeamExtremePro/ExtremeProUserbot)")
