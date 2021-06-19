9764fde3eea749f332a759ae87ea3a1a8aef4edc
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-19 12:21:49 +0530

    alivve

--- plugins/alive.py
+++ plugins/alive.py
@@ -21,22 +21,22 @@ edit_time = 5
 PHOTO = os.environ.get("EXTREMEPRO_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
 """ =======================CONSTANTS====================== """
  
-EXTREMEPRO += (
+pm_caption += (
     f"                 MY🔥MASTER\n  **『{DEFAULTUSER}』**\n\n"
 )
-EXTREMEPRO += f"┏━━━━━━━━━━━━━━━━━━━\n"
-EXTREMEPRO += f"┣•➳➠ `Telethon Version :` `1.21.1` \n"
-EXTREMEPRO += f"┣•➳➠ `ExtremePro Version :` `0.0.1`\n"
-EXTREMEPRO += f"┣•➳➠ `Python Version :` `3.9.5`\n"
-EXTREMEPRO += f"┣•➳➠ `Support :` [Support](https://t.me/ExtremePro)\n"
-EXTREMEPRO += f"┣•➳➠ `Repo🔥 :` [Repo🔥](https://github.com/TeamExtremePro/ExtremeProUserbot)\n"
-EXTREMEPRO += f"┣•➳➠ `Deploy⚡ :` [Deploy⚡Me](https://dashboard.heroku.com/new?button-url=https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FDeploy&template=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FDeploy)\n"
-EXTREMEPRO += f"┗━━━━━━━━━━━━━━━━━━━\n"
+pm_caption += f"┏━━━━━━━━━━━━━━━━━━━\n"
+pm_caption += f"┣•➳➠ `Telethon Version :` `1.21.1` \n"
+pm_caption += f"┣•➳➠ `ExtremePro Version :` `0.0.1`\n"
+pm_caption += f"┣•➳➠ `Python Version :` `3.9.5`\n"
+pm_caption += f"┣•➳➠ `Support :` [Support](https://t.me/ExtremePro)\n"
+pm_caption += f"┣•➳➠ `Repo🔥 :` [Repo🔥](https://github.com/TeamExtremePro/ExtremeProUserbot)\n"
+pm_caption += f"┣•➳➠ `Deploy⚡ :` [Deploy⚡Me](https://dashboard.heroku.com/new?button-url=https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FDeploy&template=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FDeploy)\n"
+pm_caption += f"┗━━━━━━━━━━━━━━━━━━━\n"
 
-@borg.on(extremepro_cmd(pattern="alive"))
-@borg.on(amanpandey_cmd(pattern="alive", allow_sudo=True))
+@borg.on(admin_cmd(pattern="alive"))
+@borg.on(sudo_cmd(pattern="alive", allow_sudo=True))
 async def _(event):
   await event.get_chat()
   await event.delete()
-  await borg.send_file(event.chat_id, EXTREMEPRO_PIC, caption=EXTREMEPRO)
+  await borg.send_file(event.chat_id, PHOTO, caption=pm_caption)
   await event.delete()
