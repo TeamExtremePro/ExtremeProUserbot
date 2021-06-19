6c21028835fa5bb43a5a465a8330d6ce1e93e153
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-19 12:33:47 +0530

    test

--- plugins/alive.py
+++ plugins/alive.py
@@ -7,33 +7,32 @@ Syntax: .alive
 """
 
 
-
 import asyncio
 import os
 import random
-from telethon import events
-from userbot.utils import admin_cmd, sudo_cmd
-from userbot import ALIVE_NAME
+from telethon import events, TelegramClient
+from amanpandey import extremepro_cmd
 from telethon.tl.types import ChannelParticipantsAdmins
+from amanpandey import ALIVE_NAME
+from amanpandey import extremepro_cmd, amanpandey_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ExtremePro User"
-edit_time = 5
-PHOTO = os.environ.get("EXTREMEPRO_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 """ =======================CONSTANTS====================== """
- 
-pm_caption += f"┏━━━━━━━━━━━━━━━━━━━\n"
-pm_caption += f"┣•➳➠ `Telethon Version :` `1.21.1` \n"
-pm_caption += f"┣•➳➠ `ExtremePro Version :` `0.0.1`\n"
-pm_caption += f"┣•➳➠ `Python Version :` `3.9.5`\n"
-pm_caption += f"┣•➳➠ `Support :` [Support](https://t.me/ExtremePro)\n"
-pm_caption += f"┣•➳➠ `Repo🔥 :` [Repo🔥](https://github.com/TeamExtremePro/ExtremeProUserbot)\n"
-pm_caption += f"┣•➳➠ `Deploy⚡ :` [Deploy⚡Me](https://dashboard.heroku.com/new?button-url=https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FDeploy&template=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FDeploy)\n"
-pm_caption += f"┗━━━━━━━━━━━━━━━━━━━\n"
-
-@borg.on(admin_cmd(pattern="alive"))
-@borg.on(sudo_cmd(pattern="alive", allow_sudo=True))
-async def _(event):
-  await event.get_chat()
-  await event.delete()
-  await borg.send_file(event.chat_id, PHOTO, caption=pm_caption)
-  await event.delete()
+EXTREMEPRO_PIC = os.environ.get("EXTREMEPRO_PIC", None) or "https://telegra.ph/file/75520b56df7b9159438cb.jpg"
+EXTREMEPRO = f" ┏━━━━━━━━━━━━━━━━━━━\n"
+EXTREMEPRO += f"┣•➳➠ `Telethon Version :` `1.21.1` \n"
+EXTREMEPRO += f"┣•➳➠ `ExtremePro Version :` `0.0.1`\n"
+EXTREMEPRO += f"┣•➳➠ `Python Version :` `3.9.5`\n"
+EXTREMEPRO += f"┣•➳➠ `Support :` [Support](https://t.me/ExtremePro)\n"
+EXTREMEPRO += f"┣•➳➠ `Repo🔥 :` [Repo🔥](https://github.com/TeamExtremePro/ExtremeProUserbot)\n"
+EXTREMEPRO += f"┣•➳➠ `Deploy⚡ :` [Deploy⚡Me](https://dashboard.heroku.com/new?button-url=https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FDeploy&template=https%3A%2F%2Fgithub.com%2FTeamExtremePro%2FDeploy)\n"
+EXTREMEPRO += f"┗━━━━━━━━━━━━━━━━━━━\n"
+@borg.on(extremepro_cmd(outgoing=True, pattern="alive$"))
+@borg.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+async def up(op):
+    if op.fwd_from:
+        return
+    await op.get_chat()
+    await op.delete()
+    await borg.send_file(op.chat_id, EXTREMEPRO_PIC, caption=EXTREMEPRO)
+    await op.delete() 
