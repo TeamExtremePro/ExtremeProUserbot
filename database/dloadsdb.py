f77fd6c6ff2fc045fd7edd02c74ab06439284573
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-20 22:20:38 +0530

    Create dloadsdb.py

---
+++ database/dloadsdb.py
@@ -0,0 +1,20 @@
+import logging
+from database.mongo import cli
+
+
+cli = cli["NiceGrill"]["Dloads"]
+
+
+async def dload(name, link):
+    return cli.insert_one({"Name": name, "URL": link})
+
+async def unload(name):
+    return cli.delete_one({"Name": name})
+
+async def check_dload():
+    return (False if not cli.find({"Name": {"$exists": True}})
+        else cli.find({"Name": {"$exists": True}}))
+
+async def delete(name):
+    return cli.delete_one({name: {"$exists": True}})
+        
