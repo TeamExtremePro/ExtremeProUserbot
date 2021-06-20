369b85222744057806af2a002f04ae8e443e06c1
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-20 14:37:07 +0530

    init

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,19 +5,18 @@ import os
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from amanpandey import *
-from amanpandey import extremepro_cmd, amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
-from amanpandey import Config
-from amanpandey import Var
+from Extre import *
+from Extre.utils import extremepro_cmd, amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
+from Extre.config import Config
+from Extre.variables import Var
 from Extre import CMD_LIST, CMD_HELP, CMD_HELP_BOT, BRAIN_CHECKER, INT_PLUG, LOAD_PLUG, COUNT_MSG, USERS, COUNT_PM, LASTMSG, CMD_HELP, ISAFK, AFKREASON, SUDO_LIST
 AUTONAME = os.environ.get("AUTONAME", None)
 from telethon.tl.types import Channel
 
-from amanpandey import *
-from amanpandey import ALIVE_NAME, bot, extremeprover
-from amanpandey import Config, Var
+from Extre import *
+from Extre import ALIVE_NAME, bot
 PMSECURITY = os.environ.get("PMSECURITY", None)
-
+extremeprover = "0.1"
 # stats
 if Var.PRIVATE_GROUP_ID:
     log = "Enabled"
