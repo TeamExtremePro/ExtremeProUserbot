9c8b3015ea68a0cc01661933608e9db59e6ffccc
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-20 14:50:49 +0530

    init

--- sql_helper/__init__.py
+++ sql_helper/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 # the secret configuration specific things
-from amanpandey import Var
+from Extre.variables import Var
 
 
 def start() -> scoped_session:
