49c044f8e31a09cb1100a11b2e23338fda0b5ff1
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-21 09:05:00 +0530

    mongo

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,20 @@
-import os
+import pymongo
+
+from huh import IS_ATLAS, MONGO_URL, DB_NAME
+
+
+def database():
+    """Created Database connection"""
+    if IS_ATLAS:
+        client = pymongo.MongoClient(
+            MONGO_URL,
+        )
+    else:
+        from userbot import DB_USERNAME, DB_PASSWORD
+
+        client = pymongo.MongoClient(
+            MONGO_URL, username=DB_USERNAME, password=DB_PASSWORD
+        )
+
+    db = client[DB_NAME]
+    return db
