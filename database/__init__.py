f728957850f3eef2a9ed65c7581c83bfeff837e1
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-25 09:32:09 +0530

    inti

--- database/__init__.py
+++ database/__init__.py
@@ -1,7 +1,11 @@
+import asyncio
+import sys
 import pymongo
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 from huh import IS_ATLAS, MONGO_URI, DB_NAME
-
+from motor import motor_asyncio
 
 def database():
     """Created Database connection"""
@@ -18,3 +22,8 @@ def database():
 
     db = client[DB_NAME]
     return db
+
+try:
+    asyncio.get_event_loop().run_until_complete(motor.server_info())
+except ServerSelectionTimeoutError:
+    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
