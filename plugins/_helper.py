f02e66db4e79dd5cbfa9097827b230ffeae14593
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-26 12:33:09 +0530

    fixed old onle

--- plugins/_helper.py
+++ plugins/_helper.py
@@ -1,75 +1,72 @@
 import os
-from Extre import CMD_LIST, ALIVE_NAME, CMD_HELP
-from Extre.utils import admin_cmd
-from Extre.utils import edit_or_reply
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Extreme PRo User"
 
-CUSTM_HLP_EMOJ = os.environ.get("CUSTM_HLP_EMOJ", " ")
+from Extre import ALIVE_NAME, CMD_HELP, CMD_LIST
+from Extre.config import Config
+from Extre.utils import extremepro_cmd
 
- #@command(pattern="^.help ?(.*)")
-@borg.on(admin_cmd(pattern=r"help ?(.*)"))
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "EXTREMEPRO USER"
+CMD_HNDLR = os.environ.get("CMD_HNDLR", None)
+CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "⚡")
 
+if CMD_HNDLR is None:
+    CMD_HNDLR = "."
+
+
+@bot.on(extremepro_cmd(pattern="help ?(.*)"))
+@bot.on(amanpandey_cmd(pattern="help ?(.*)", allow_sudo=True))
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
         input_str = event.pattern_match.group(1)
         if tgbotusername is None or input_str == "text":
             string = ""
-            for i in CMD_LIST:
-                string += "✨ " + i + "\n"
-                for iter_list in CMD_LIST[i]:
+            for i in CMD_HELP:
+                string += CUSTOM_HELP_EMOJI + " " + i + " " + CUSTOM_HELP_EMOJI + "\n"
+                for iter_list in CMD_HELP[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
                 with io.BytesIO(str.encode(string)) as out_file:
                     out_file.name = "cmd.txt"
-                    await bot.send_file(
+                    await tgbot.send_file(
                         event.chat_id,
                         out_file,
                         force_document=True,
                         allow_cache=False,
                         caption="**COMMANDS**",
-                        reply_to=reply_to_id
+                        reply_to=reply_to_id,
                     )
                     await event.delete()
             else:
                 await event.edit(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "Commands found in {}:".format(input_str)
-                for i in CMD_LIST[input_str]:
-                    string += "    " + i
-                    string += "\n"
-                await event.edit(string)
+                string = "**Cσɱɱαɳԃʂ αʋαιʅαႦʅҽ ιɳ {}** \n\n".format(input_str)
+                if input_str in CMD_HELP:
+                    for i in CMD_HELP[input_str]:
+                        string += i
+                    string += "\n\n**© @Extremepro_UserbotBotSupport**"
+                    await event.edit(string)
+                else:
+                    for i in CMD_LIST[input_str]:
+                        string += "    " + i
+                        string += "\n"
+                    string += "\n**© @EXTREMEPRO_USERBOT**"
+                    await event.edit(string)
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""вσт σƒ {DEFAULTUSER}\n\n ⚙️•єχтяємєρяσ υѕєявσт•⚙️\n\n"""
-            results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername,
-                help_string
-            )
-            await results[0].click(
-                event.chat_id,
-                reply_to=event.reply_to_msg_id,
-                hide_via=True
-            )
-            await event.delete()
-
-@borg.on(admin_cmd(outgoing=True, pattern="info ?(.*)"))
-async def info(event):
-    """ For .info command,"""
-    args = event.pattern_match.group(1).lower()
-    if args:
-        if args in CMD_HELP:
-            await edit_or_reply(event, str(CMD_HELP[args]))
-        else:
-            await edit_or_reply(event, "Please specify a valid plugin name.")
-    else:
-        string = "**Please specify which plugin do you want help for !!**\
-            \n**Usage:** `.info` <plugin name>\n\n"
-        for i in sorted(CMD_HELP):
-            string += "🔹`" + str(i)
-            string += "`   "
-        await edit_or_reply(event, string)
+            help_string = f"""вσт σƒ {DEFAULTUSER}\n\n ⚙️•єχтяємєρяσ υѕєявσт•⚙️ **[ExtremeProUSerbot](@EXTREMEPRO_USERBOT)**\n\n"""
+            try:
+                results = await bot.inline_query(  # pylint:disable=E0602
+                    tgbotusername, help_string
+                )
+                await results[0].click(
+                    event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+                )
+                await event.delete()
+            except BaseException:
+                await event.edit(
+                    f"тнιѕ вσт нαѕ ιηℓιηє ∂ιѕαвℓє∂. ρℓєαѕє єηαвℓє ιт тσ υѕє `{CMD_HNDLR}help`.\nGet help from [here](t.me/EXTREMEPRO_USERBOT)"
+                )
