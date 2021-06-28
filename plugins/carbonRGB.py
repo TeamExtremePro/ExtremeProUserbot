2e334046e41f0fd5e8f5aa5b4aa1db74b3f81370
amanpandey7647 <amanpandey7647@users.noreply.github.com>
2021-06-28 11:05:40 +0000

    Replaced new name

--- plugins/carbonRGB.py
+++ plugins/carbonRGB.py
@@ -108,7 +108,7 @@ async def carbon_api(e):
         await e.client.send_file(
             e.chat_id,
             file,
-            caption="✅Carbonised by [ExtremeProUserbot](https://github.com/indianbhaiya/ExtremeProUserbot/)",
+            caption="✅Carbonised by [ExtremeProUserbot](https://github.com/TeamExtremePro/ExtremeProUserbot/)",
             force_document=False,
             reply_to=e.message.reply_to_msg_id,
         )
--- plugins/updater.py
+++ plugins/updater.py
@@ -16,7 +16,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/indianbhaiya/ExtremeProUserbotBot"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/TeamExtremePro/ExtremeProUserbotBot"
 BOT_IS_UP_TO_DATE = "the userbot is up-to-date."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n" "changelog: \n\n{changelog}\n" "updating ..."
