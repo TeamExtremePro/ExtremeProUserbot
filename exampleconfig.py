a6034a54fb90ef0915c21a0e952f320d27f2e7ae
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-06-29 16:37:54 +0530

    example config

---
+++ exampleconfig.py
@@ -0,0 +1,26 @@
+from Extre.config import Config
+
+
+class Development(Config):
+    # get this values from the my.telegram.org
+    APP_ID = 6
+    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    # the name to display in your alive message
+    ALIVE_NAME = "Your value"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "Your value"
+    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+    STRING_SESSION = "Your value"
+    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
+    TG_BOT_TOKEN_BF_HER = "Your value"
+    TG_BOT_USER_NAME_BF_HER = "Your value"
+    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
+    PRIVATE_GROUP_BOT_API_ID = -100
+    # command handler
+    COMMAND_HAND_LER = "."
+    # sudo enter the id of sudo users userid's in that array
+    SUDO_USERS = []
+    # Alive Pic
+    EXTREMEPRO_PIC = ""
+    # Mongo Db Url Required
+    MONGO_URI = ""
