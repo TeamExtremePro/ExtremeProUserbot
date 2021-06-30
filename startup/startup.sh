6dd7f23cba2ab22fbca1c268c0082e2658557991
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-06-30 18:46:16 +0530

    startup.sh

---
+++ startup/startup.sh
@@ -0,0 +1,15 @@
+ 
+echo "
+ 
+        +-+ +-+ +-+ +-+ +-+ +-+ +-+
+        EXTREMEPRO USERBOT
+        +-+ +-+ +-+ +-+ +-+ +-+ +-+
+ 
+ 
+      Visit @TeamExtremePro for updates!!
+"
+
+
+pip3 install -U -r start-requirements.txt
+pip3 install --upgrade Extre
+python3 -m Extre
