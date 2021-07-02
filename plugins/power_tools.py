4c814ce7863cc967c5fa67c88c9d4218b1e6e049
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-02 12:02:27 +0530

    noe

--- plugins/power_tools.py
+++ plugins/power_tools.py
@@ -16,12 +16,9 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    # await asyncio.sleep(2)
-    # await event.edit("Restarting [██░] ...\n`.awake` me or `.helpme` to check if I am online")
-    # await asyncio.sleep(2)
-    # await event.edit("Restarting [███]...\n`.awake` me or `.helpme` to check if I am online")
-    # await asyncio.sleep(2)
-    await event.edit("Restarted. `.awake` me or `.helpme` to check if I am online")
+    await event.edit("✳  🎀  𝑅𝑒𝓈𝓉𝒶𝓇𝓉𝒾𝓃𝑔 \𝓃𝒟❤`.𝒶𝓁𝒾𝓋𝑒`  🌺𝓇 `.𝒽𝑒𝓁𝓅𝓂𝑒` 𝒶𝒻𝓉𝑒𝓇 𝟧 𝓂𝒾𝓃𝓊𝓉𝑒𝓈 🍑𝒻 𝓇𝑒𝓈𝓉𝒶𝓇𝓉𝒾𝓃𝑔 𝓅𝓇😍𝒸𝑒𝓈𝓈 𝓉🍪 𝒸𝒽𝑒𝒸𝓀 𝒾𝒻 𝐼 𝒶𝓂 💍𝓃𝓁𝒾𝓃𝑒  🎀  ✳")
+    await asyncio.sleep(2)
+    await event.edit("ℜ𝔢𝔰𝔱𝔞𝔯𝔱𝔢𝔡 𝔜𝔬𝔲𝔯 𝔅𝔬𝔱✅ \n𝔇𝔬`.𝔞𝔩𝔦𝔳𝔢`  𝔬𝔯 `.𝔥𝔢𝔩𝔭𝔪𝔢` 𝔞𝔣𝔱𝔢𝔯 5 𝔪𝔦𝔫𝔲𝔱𝔢𝔰 𝔬𝔣 𝔯𝔢𝔰𝔱𝔞𝔯𝔱𝔦𝔫𝔤 𝔭𝔯𝔬𝔠𝔢𝔰𝔰 𝔱𝔬 𝔠𝔥𝔢𝔠𝔨 𝔦𝔣 ℑ 𝔞𝔪 𝔬𝔫𝔩𝔦𝔫𝔢")
     await borg.disconnect()
     # https://archive.is/im3rt
     os.execl(sys.executable, sys.executable, *sys.argv)
