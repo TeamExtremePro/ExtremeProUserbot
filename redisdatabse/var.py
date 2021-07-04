2423416be8b1cf7f4f76b3db846d5cad8449b295
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-04 19:34:19 +0530

    var

--- redisdatabse/var.py
+++ redisdatabse/var.py
@@ -4,7 +4,8 @@ import os
 class Var:
     API_ID = os.environ.get("APP_ID")
     API_HASH = os.environ.get("API_HASH")
-    TG_BOT_TOKEN_BF_HER = os.environ.get("BOT_TOKEN")
+    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
     STRING_SESSION = os.environ.get("STRING_SESSION")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
     HEROKU_API_KEY = os.environ.get("HEROKU_API")
