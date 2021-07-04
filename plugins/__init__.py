f3305ef48b9f5ecdb91613a50cfef80b272e21eb
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-04 19:35:10 +0530

    init

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -9,6 +9,7 @@ from redisdatabse.connections import client_connection, redis_connection
 from Extre import *
 from Extre.utils import extremepro_cmd, amanpandey_cmd, load_module, humanbytes, register, command, start_assistant, errors_handler, progress, human_to_bytes, time_formatter, is_admin
 from Extre.config import Config
+from redisdatabse.var import Var
 from Extre.variables import Var
 from Extre import CMD_LIST, CMD_HELP, CMD_HELP_BOT, BRAIN_CHECKER, INT_PLUG, LOAD_PLUG, COUNT_MSG, USERS, COUNT_PM, LASTMSG, CMD_HELP, ISAFK, AFKREASON, SUDO_LIST
 AUTONAME = os.environ.get("AUTONAME", None)
