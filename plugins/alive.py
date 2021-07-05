fd40655e7df12dff8f17ccf99c3e5ca0e5f6f350
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-05 07:02:46 +0530

    Alive

--- plugins/alive.py
+++ plugins/alive.py
@@ -13,7 +13,7 @@ from telethon import events, TelegramClient
 from Extre import *
 from Extre.utils import extremepro_cmd
 from telethon.tl.types import ChannelParticipantsAdmins
-from Extre import ALIVE_NAME
+from Extre import ALIVE_NAME,. StartTime as uptm
 from Extre.utils import extremepro_cmd as extremepro_cmd, amanpandey_cmd as amanpandey_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
