3dc681e9ea62a718de5a40a9f7aa80ff8c9f49f2
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-05 18:05:16 +0530

    test

--- plugins/power_tools.py
+++ plugins/power_tools.py
@@ -12,18 +12,15 @@ import sys
 from uniborg.util import admin_cmd
 
 
+
+
 @borg.on(admin_cmd(pattern="restart"))
-async def _(event):
-    if event.fwd_from:
-        return
-    await event.edit("✳  🎀  𝑅𝑒𝓈𝓉𝒶𝓇𝓉𝒾𝓃𝑔 \𝓃𝒟❤`.𝒶𝓁𝒾𝓋𝑒`  🌺𝓇 `.𝒽𝑒𝓁𝓅𝓂𝑒` 𝒶𝒻𝓉𝑒𝓇 𝟧 𝓂𝒾𝓃𝓊𝓉𝑒𝓈 🍑𝒻 𝓇𝑒𝓈𝓉𝒶𝓇𝓉𝒾𝓃𝑔 𝓅𝓇😍𝒸𝑒𝓈𝓈 𝓉🍪 𝒸𝒽𝑒𝒸𝓀 𝒾𝒻 𝐼 𝒶𝓂 💍𝓃𝓁𝒾𝓃𝑒  🎀  ✳")
-    await asyncio.sleep(2)
-    await event.edit("ℜ𝔢𝔰𝔱𝔞𝔯𝔱𝔢𝔡 𝔜𝔬𝔲𝔯 𝔅𝔬𝔱✅ \n𝔇𝔬`.𝔞𝔩𝔦𝔳𝔢`  𝔬𝔯 `.𝔥𝔢𝔩𝔭𝔪𝔢` 𝔞𝔣𝔱𝔢𝔯 5 𝔪𝔦𝔫𝔲𝔱𝔢𝔰 𝔬𝔣 𝔯𝔢𝔰𝔱𝔞𝔯𝔱𝔦𝔫𝔤 𝔭𝔯𝔬𝔠𝔢𝔰𝔰 𝔱𝔬 𝔠𝔥𝔢𝔠𝔨 𝔦𝔣 ℑ 𝔞𝔪 𝔬𝔫𝔩𝔦𝔫𝔢")
-    await borg.disconnect()
-    # https://archive.is/im3rt
-    os.execl(sys.executable, sys.executable, *sys.argv)
-    # You probably don't need it but whatever
-    quit()
+async def restartbt(ult):
+    ok = await eor(ult, "ℜ𝔢𝔰𝔱𝔞𝔯𝔱𝔢𝔡 𝔜𝔬𝔲𝔯 𝔅𝔬𝔱✅ \n𝔇𝔬`.𝔞𝔩𝔦𝔳𝔢`  𝔬𝔯 `.𝔥𝔢𝔩𝔭𝔪𝔢` 𝔞𝔣𝔱𝔢𝔯 5 𝔪𝔦𝔫𝔲𝔱𝔢𝔰 𝔬𝔣 𝔯𝔢𝔰𝔱𝔞𝔯𝔱𝔦𝔫𝔤 𝔭𝔯𝔬𝔠𝔢𝔰𝔰 𝔱𝔬 𝔠𝔥𝔢𝔠𝔨 𝔦𝔣 ℑ 𝔞𝔪 𝔬𝔫𝔩𝔦𝔫𝔢")
+    if Var.HEROKU_API:
+        await restart(ok)
+    else:
+        await bash("pkill python3 && python3 -m Extre")
 
 
 @borg.on(admin_cmd(pattern="shutdown"))
