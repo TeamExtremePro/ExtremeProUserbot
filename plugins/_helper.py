1a72d7e03627405b2e249875688bf719ce858571
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-06 12:35:54 +0530

    sudo

--- plugins/_helper.py
+++ plugins/_helper.py
@@ -59,3 +59,28 @@ async def _(event):
     await event.edit(result.stringify())
 
 
+
+@borg.on(sudo_cmd(allow_sudo=True, pattern="help(?: |$)(.*)"))
+async def info(event):
+    if event.fwd_from:
+        return
+    """ For .info command,"""
+    args = event.pattern_match.group(1).lower()
+    input_str = event.pattern_match.group(1)
+    if args:
+        if args in SUDO_LIST:
+            if input_str in SUDO_LIST:
+                string = "Commands found in {}:\n".format(input_str)
+                for i in SUDO_LIST[input_str]:
+                    string += "    " + i
+                    string += "\n"
+                await event.reply(string)
+        else:
+                await event.reply(args + " is not a valid plugin!")
+    else:
+        string = "**Please specify which plugin do you want help for !!**\
+            \n**Usage:** `.info` <plugin name>\n\n"
+        for i in sorted(SUDO_LIST):
+            string += "◆`" + str(i)
+            string += "`   "
+        await event.reply(string)
